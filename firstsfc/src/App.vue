818bd0bc89b590fab72bb8ad69165e9d9e2e91cf
LAPTOP-J28AGGVS\ciberchema <ciberchema@gmail.com>
2024-05-03 14:03:58 +0200

    props exercises

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -10,11 +10,43 @@ export default {
 
 <template>
   <h1>{{ message }}</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
+  <p>My favorite food has a diploma image attached to id.</p>
+  <div class="wrapper">
+    <food-item 
+      food-name="Apples"
+      food-desc="Apples are a type of fruit that grow in trees."
+      v-bind:is-favorite="true"
+    />
+    <food-item 
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, chees, and topping on top."
+      v-bind:is-favorite="false"
+    />
+    <food-item 
+      food-name="Rice"
+      v-bind:is-favorite="false"
+    />
+  </div>
 </template>
 
 <style>
+    .wrapper {
+        display: flex;
+        flex-wrap: wrap;
+    }
 
+    .wrapper div{
+        border: dashed black 1px;
+
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+    .wrapper > div:hover{
+        cursor:pointer;
+    }
+    #red {
+        font-weight: bold;
+        color: red;
+    }
 </style>
--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,41 +1,69 @@
 <template>
-<div @:click="countClicks()">
-        <h2>{{ name }}</h2>
+    <div @:click="countClicks()">
+        <h2>{{ foodName }}</h2>
+        <img src="/approval.jpg" v-show="foodIsFavorite" alt="imagen diploma"/>
         <p>{{ message }}</p>
+        <p>{{ foodDesc }}</p>
         <p id="red">You have clicked me {{ clicks }} times.</p>
+        <button @:click="toggleFavorite()">Favorite</button>
     </div>
 </template>
 
 <script>
     export default {
+        // props: ['foodName','foodDesc','isFavorite'] //props como string por defecto
+       /* 
+        objeto props sencillo 
+        props: {
+            foodName: String,
+            foodDesc: String,
+            isFavorite: Boolean
+        },*/
+        props: {
+            foodName: {
+                type: String,
+                required: true
+            },
+            foodDesc: {
+                type: String,
+                required: false,
+                default: 'This is the default description',
+                validator: (value) => { //arrow function sustituye a funcion (value)
+                    if( 20 < value.length && value.length <50){
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            },
+            isFavorite:{
+                type: Boolean,
+                required: false,
+                default: false
+            }
+        },
         data(){
             return{
-                name: 'Apples',
-                message: 'I like apples',
-                clicks: 0
+                message: 'I like '+ this.foodName,
+                clicks: 0,
+                foodIsFavorite: this.isFavorite
             }
         },
         methods: {
             countClicks(){
                 this.clicks++;
+            },
+            toggleFavorite(){
+                this.foodIsFavorite = !this.foodIsFavorite;
             }
         }
     };
 </script>
 
 <style>
-    #app > div{
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightgreen;
+    img {
+        height: 5rem;
+        float: right;
     }
-    #app > div:hover{
-        cursor:pointer;
-    }
-    #red {
-        font-weight: bold;
-        color: red;
-    }
+   
 </style>
