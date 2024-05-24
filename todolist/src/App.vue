02476b82569b9ec13039ded88e46a10b5d387202
LAPTOP-J28AGGVS\ciberchema <ciberchema@gmail.com>
2024-05-24 16:21:58 +0200

    todo list

---
+++ todolist/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+  export default {
+    data(){
+      return {
+        newItem: '',
+        items: ['Buy apples','Make pizza','Mow the lawn']
+      };
+    },
+    methods: {
+      addItem(){
+        this.items.push(this.newItem),
+        this.newItem = '';
+      }
+    }
+  }
+</script>
+
+<template>
+  <h3>Todo List</h3>
+  <ul>
+    <todo-item 
+      v-for="x in items"
+      :key="x"
+      :item-name="x"
+      style="background-color: lightgreen;"
+    />
+  </ul>
+  <input v-model="newItem"/>
+  <button @click="addItem">Add</button>
+</template>
+
+<style>
+  ul{
+    width: 150px;
+    list-style-type: none;
+    padding-left: 10px;
+  }
+</style>
